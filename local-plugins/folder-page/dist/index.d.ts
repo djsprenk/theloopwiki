83342989497ad6df05af8f880eb29b613c860845
djsprenk <code@djsprenk.com>
2026-07-07 14:47:50 -0400

    feat: update folders to point to folder index pages

---
+++ local-plugins/folder-page/dist/index.d.ts
@@ -0,0 +1,3 @@
+export { F as FolderPage, a as FolderPageOptions } from './types-Bxq1-vQW.js';
+export { _ as FolderContent } from './FolderContent-B31uKzH_.js';
+export { PageGenerator, PageMatcher, QuartzComponent, QuartzComponentConstructor, QuartzComponentProps, QuartzPageTypePlugin, QuartzPageTypePluginInstance, VirtualPage } from '@quartz-community/types';
