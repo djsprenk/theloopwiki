44845d49be49a579da37de895f250552dc487a61
djsprenk <code@djsprenk.com>
2026-07-07 17:32:02 -0400

    feat: update header for mobile

---
+++ local-plugins/page-title/src/components/PageTitle.tsx
@@ -0,0 +1,35 @@
+import type {
+  QuartzComponent,
+  QuartzComponentProps,
+  QuartzComponentConstructor,
+} from "@quartz-community/types";
+import { classNames } from "../util/lang";
+import { pathToRoot } from "../util/path";
+import { i18n } from "../i18n";
+
+const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
+  const locale = cfg?.locale ?? "en-US";
+  const title = cfg?.pageTitle ?? i18n(locale).propertyDefaults.title;
+  const baseDir = pathToRoot(fileData.slug as string);
+  const words = title.trim().split(" ");
+  const lastWord = words.pop();
+  const leadingWords = words.join(" ");
+  return (
+    <h2 class={classNames(displayClass, "page-title")}>
+      <a href={baseDir}>
+        {leadingWords && <span class="page-title-leading">{leadingWords} </span>}
+        <span class="page-title-last">{lastWord}</span>
+      </a>
+    </h2>
+  );
+};
+
+PageTitle.css = `
+.page-title {
+  font-size: 1.75rem;
+  margin: 0;
+  font-family: var(--titleFont);
+}
+`;
+
+export default (() => PageTitle) satisfies QuartzComponentConstructor;
